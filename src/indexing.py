2da610b05703ca725787f77cadecad9778f3dc95
RGiles8 <121770685+RGiles8@users.noreply.github.com>
2026-01-29 17:17:25 -0500

    Create indexing.py

---
+++ src/indexing.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+from typing import Dict, Iterable, List, Tuple
+
+from step1_preprocessing import iter_scifact_tokens
+
+
+def build_inverted_index(
+    token_stream: Iterable[Tuple[str, List[str]]]
+) -> Dict[str, Dict[str, int]]:
+    inverted_index: Dict[str, Dict[str, int]] = defaultdict(dict)
+
+    for doc_id, tokens in token_stream:
+        term_freq = defaultdict(int)
+
+        for token in tokens:
+            term_freq[token] += 1
+
+        for term, freq in term_freq.items():
+            inverted_index[term][doc_id] = freq
+
+    return inverted_index
+
+if __name__ == "__main__":
+    print("STEP 2: Inverted index sample")
+
+    index = build_inverted_index(iter_scifact_tokens())
+
+    for term in list(index.keys())[:5]:
+        print(term, index[term])
